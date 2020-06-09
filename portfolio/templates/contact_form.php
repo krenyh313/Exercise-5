ed481c2af03d6cd767250cccd6d1bbbf48f456e7
krenyh313 <krenyh313@gmail.com>
2020-06-09 08:44:28 +0300

    Personal Portfolio update

--- portfolio/templates/contact_form.php
+++ portfolio/templates/contact_form.php
@@ -57,7 +57,7 @@
             $headers = 'From: ' . $email . "\r\n" .
             'Reply-To: ' . $email . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
-            @mail($email_to, $email_subject, $email_message, $headers);
+            mail($email_to, $email_subject, $email_message, $headers);
         ?> 
     </section>
 </body>
